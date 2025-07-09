9ca088ec621701bc3a472614692b985929376439
HARSHIT PANDEY <121718495+harshit-oss@users.noreply.github.com>
2025-07-10 02:49:35 +0530

    Update churnshield_chatbot.py

--- churnshield_chatbot.py
+++ churnshield_chatbot.py
@@ -121,7 +121,3 @@ if query:
 
     else:
         st.warning("🤖 Sorry, I didn’t understand that. Try selecting a question above.")
-import streamlit as st
-
-st.sidebar.markdown("### 👨‍💻 Developed by Harshit Pandey")
-st.sidebar.markdown("[GitHub](https://github.com/yourprofile) | [LinkedIn](https://linkedin.com/in/yourprofile)")
